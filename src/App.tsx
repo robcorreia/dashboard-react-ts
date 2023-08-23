aa4b8dbc9a1031c726f91e00047ed4e082665aad
Robson <robccorreia@gmail.com>
2023-08-23 10:43:09 -0300

    feat: add pages structure and useFetch hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css'
+import Header from './components/Header'
+import SideNav from './components/SideNav'
+import Summary from './pages/Summary'
 
 function App() {
 
   return (
     <div>
+      <SideNav />
+      <main>
+        <Header />
+        <Summary />
+      </main>
     </div>
 
   )
