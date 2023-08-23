3e96e76c662f701ef9b03056513719089e22d9ea
Robson <robccorreia@gmail.com>
2023-08-23 15:11:58 -0300

    feat: add input styles

--- src/components/DateInput/index.tsx
+++ src/components/DateInput/index.tsx
@@ -1,5 +1,26 @@
 import React from 'react'
 
+const generalStyle: React.CSSProperties = {
+  fontSize: '1rem',
+  color: 'var(--color-2)',
+  padding: 'var(--gap-sm) .75rem',
+  background: 'var(--color-4)',
+  borderRadius: 'var(--gap-sm)'
+}
+
+const labelStyle: React.CSSProperties = {
+  display: 'block',
+  marginBottom: 'var(--gap-sm)',
+  fontWeight: 600,
+  ...generalStyle
+}
+
+const inputStyle: React.CSSProperties = {
+  border: 'none',
+  fontFamily: 'monospace',
+  ...generalStyle
+}
+
 type IDataInput = React.ComponentProps<'input'> & {
   label: string;
   id: string;
@@ -9,8 +30,8 @@ const DateInput = ({ id, label, ...rest }: IDataInput) => {
 
   return (
     <div>
-      <label htmlFor={id}>{label}</label>
-      <input id={id} type="date" {...rest} />
+      <label style={labelStyle} htmlFor={id}>{label}</label>
+      <input style={inputStyle} id={id} type="date" {...rest} />
     </div>
   )
 }
