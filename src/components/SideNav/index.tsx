e91c92273da04ee4a39c026c9e002899f8e333aa
Robson <robccorreia@gmail.com>
2023-08-26 11:25:03 -0300

    feat: add sidenav

--- src/components/SideNav/index.tsx
+++ src/components/SideNav/index.tsx
@@ -1,8 +1,37 @@
+import { ArrowsDownUp, EnvelopeSimple, GearSix, House, SignOut, Storefront } from '@phosphor-icons/react'
 import React from 'react'
 
 const SideNav = () => {
   return (
-    <div>SideNav</div>
+    <nav className='sidenav box bg-3'>
+      <a className='brand' href="/">Fintech</a>
+      <ul>
+        <li>
+          <div><House size={20} weight="bold" /></div>
+          <a href="">Resumo</a>
+        </li>
+        <li>
+          <div><Storefront size={20} weight="bold" /></div>
+          <a href="">Vendas</a>
+        </li>
+        <li>
+          <div><Storefront size={20} weight="bold" /></div>
+          <a>Webhooks</a>
+        </li>
+        <li>
+          <div><ArrowsDownUp size={20} weight="bold" /></div>
+          <a>Configurações</a>
+        </li>
+        <li>
+          <div><EnvelopeSimple size={20} weight="bold" /></div>
+          <a>Contato</a>
+        </li>
+        <li>
+          <div><SignOut size={20} weight="bold" /></div>
+          <a>Sair</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
